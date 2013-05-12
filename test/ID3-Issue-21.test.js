a3c22808c9c803def96cac15668bc0718c7b7142
John Guidry <lowdowndirtyrottenscoundrel@gmail.com>
2013-05-12 16:44:57 -0500

    Add another test batch for ID3 edge case

---
+++ test/ID3-Issue-21.test.js
@@ -0,0 +1,31 @@
+var vows   = require('vows');
+var should = require('should');
+var path = require('path');
+var fs = require('fs');
+
+var ID3 = require('../lib/ID3.js');
+
+var issue21 = path.join('test','data','issue_21.id3');
+
+// Read more on this issue at
+// https://code.google.com/p/mutagen/issues/detail?id=21
+vows
+  .describe('ID3 Issue 21')
+  .addBatch({
+    'An ID3 wrapper ': {
+      'loading a file with an improper extended flag': {
+        topic: new ID3(issue21),
+        'should not set the extended flag': function (id3) {
+          id3.f_extended.should.eql(false);
+        },
+        'should have TIT2 and TALB frames': function (id3) {
+          id3.should.have.property('TIT2');
+          id3.should.have.property('TALB');
+        },
+        'should have a properly decoded value for TIT2': function (id3) {
+          should.equal(id3['TIT2'], 'Punk To Funk');
+        }
+      }
+    }
+  })
+  .export(module);
